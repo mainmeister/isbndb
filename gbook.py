187a55677e0b53874277c400118ec743a7c1052c
William R Main <mainmeister@users.noreply.github.com>
2024-06-22 18:11:59 -0400

    Add files via upload
    added new command line args

--- gbook.py
+++ gbook.py
@@ -10,10 +10,17 @@ class gbooks():
     googlebookapi: str = "https://www.googleapis.com/books/v1/volumes"
 
 
-    def search(self, value: str) -> iter:
+    def search(self, value: str, debug: bool = False, new: bool = False) -> iter:
         params: dict = {"q": value, 'key': self.googleapikey}
         r: requests.Response = requests.get(url=self.googlebookapi, params=params)
-        rj: json = r.json()
+        if debug:
+            if new:
+                openmode = 'w'
+            else:
+                openmode = 'a'
+            with open("gbook.txt", openmode) as file:
+                file.write(r.text)
+        rj: json = json.loads(r.text)
         try:
             for i in rj["items"]:
                 yield i["volumeInfo"]
@@ -22,12 +29,18 @@ class gbooks():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='gbook.py', description='Search Google Books', epilog='Example: echo test | gbook.py -pretty')
-    parser.add_argument('-p', '--pretty', help='pretty print the output', required=False, action='store_true')
+    parser = argparse.ArgumentParser(prog='gbook.py', description='Search Google Books',
+                                     epilog='Example: echo test | gbook.py --pretty --debug')
+    parser.add_argument('-p', '--pretty', help='pretty print the output',
+                        required=False, action='store_true')
+    parser.add_argument('-d', '--debug', help='write output to gbook.txt',
+                        required=False, action='store_true')
+    parser.add_argument('-n', '--new', help='create a new gbook.txt instead of appending',
+                        required=False, action='store_true')
     args = parser.parse_args()
     books = gbooks()
     for line in sys.stdin:
-        for book in books.search(line):
+        for book in books.search(line, args.debug, args.new):
             if args.pretty:
                 print(json.dumps(book, indent=4))
             else:
